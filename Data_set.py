049f3883dfc66dca86b1289bf4cc8fa5cefd695d
TriNguyenHuu2608 <huutri26081998@gmail.com>
2020-12-16 02:32:38 +0700

    Adding new files

--- Data_set.py
+++ Data_set.py
@@ -2,9 +2,15 @@ import numpy as np
 import pandas as pd
 import os
 from sklearn import preprocessing
-import feature_selection
 
-class Get_data:
+# class Get_data:
     
-    def __init__(self,):
+#     def __init__(self,):
+
+for (root,dirs,files) in os.walk ('//ĐCLV', topdown= True):
+    print('root: ', root)
+    print('dirs: ', dirs)
+    print('files: ', files)
+
+test =123123213
         
