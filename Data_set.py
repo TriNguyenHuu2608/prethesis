f041fa4157b98dffe9c415c111422185d92be174
TriNguyenHuu2608 <huutri26081998@gmail.com>
2020-12-16 02:34:36 +0700

    Adding new files

--- Data_set.py
+++ Data_set.py
@@ -1,16 +1,37 @@
 import numpy as np
 import pandas as pd
 import os
+import re
 from sklearn import preprocessing
 
-# class Get_data:
-    
-#     def __init__(self,):
+class Data_set:
+    '''
+    Class use to get train and test data.\n
+    Param : Name of substance.
+    '''
+    def __init__(self,name):
+        self.name = name
+        self.X_train = np.array([])
+        self.Y_train = np.array([])
+        self.X_test = np.array([])
+        self.Y_test = np.array([])
 
-for (root,dirs,files) in os.walk ('//ĐCLV', topdown= True):
-    print('root: ', root)
-    print('dirs: ', dirs)
-    print('files: ', files)
-
-test =123123213
+    def get_data_ga(self):
+        '''
+        Get GA data.
+        '''
+        for (root,dirs,files) in os.walk ('data_luanvan', topdown= True): #loop through files in folder
+            for file in files:
+                if self.name in file:
+                    data_concentration = float((file.split(" ")[1]).split('.xlsx')[0]) #Detect Concentration_data ex:1.5 with "H2S 1.5"
+                    df = pd.read_excel(f'{root}/{file}')
+                    df_row_load = len(df)-1 if len(df) < 5 else len(df) -2
+                    x_train = df.iloc[0:df_row_load,:200].values  
+                    y_train = np.full(df_row_load,data_concentration)
+                    if len(self.X_train) == 0:
+                        self.X_train = x_train
+                    else:
+                        self.X_train = np.vstack([self.X_train,x_train])
+                    self.Y_train = np.append(self.Y_train,y_train)
         
+        return self.X_train,self.Y_train
