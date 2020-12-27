85eeae455c20856c1c23e50ca3dfe9e709c37333
TriNguyenHuu2608 <huutri26081998@gmail.com>
2020-12-27 22:19:59 +0700

    Upload file 27/12

--- Data_set.py
+++ Data_set.py
@@ -20,14 +20,18 @@ class Data_set:
         '''
         Get GA data.
         '''
+        value_dict =   {'NH3': 10,
+                        'NO3' : 3.333,
+                        'H2S' : 2}
+
         for (root,dirs,files) in os.walk ('data_luanvan', topdown= True): #loop through files in folder
             for file in files:
-                if self.label in file:
+                if self.label in file and '~' not in file:
                     data_concentration = float((file.split(" ")[1]).split('.xlsx')[0]) #Detect Concentration_data ex:1.5 with "H2S 1.5"
                     df = pd.read_excel(f'{root}/{file}')
                     df_row_load = len(df)#-1 if len(df) < 5 else len(df) -2
                     # print('df_row_load: ', df_row_load)
-                    x_train = df.iloc[0:df_row_load,100:].values  
+                    x_train = df.iloc[0:df_row_load,100:200].values  
                     y_train = np.full(df_row_load,data_concentration)
                     if len(self.X_train) == 0:
                         self.X_train = x_train
@@ -35,6 +39,6 @@ class Data_set:
                         self.X_train = np.vstack([self.X_train,x_train])
                     self.Y_train = np.append(self.Y_train,y_train)
         
-        self.Y_train = self.Y_train * 10 #NH3
+        self.Y_train = (self.Y_train * value_dict.get(self.label))
 
         return self.X_train,self.Y_train
